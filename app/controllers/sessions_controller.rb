6456753ae1f2f4f1ba2ba0ae7d566bcc7f56b853
se4weed <norisama1159@gmail.com>
2024-03-07 19:37:12 +0900

    ログイン処理の修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: session_params[:email])
     
-    if user && user.authenticate(params[:password])
+    if user&.authenticate(session_params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "ログインしました。"
     else
-      render :new, notice: "ログインに失敗しました。"
+      redirect_to login_url, alert: "ログインに失敗しました。"
     end
   end
 
