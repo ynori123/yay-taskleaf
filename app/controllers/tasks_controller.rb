84938d477b3697d0f2b2b28b55044ba683218978
se4weed <norisama1159@gmail.com>
2024-03-07 19:45:05 +0900

    idからタスクを取得する処理を共通化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destory]
+
   def index
     @tasks = current_user.tasks.recent
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -21,17 +22,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
     task.update!(task_params)
     redirect_to tasks_url, notice: "タスク「 #{task.name} 」を更新しました。"
   end
 
   def destroy
-    task = Task.find(params[:id])
     task.destroy
     redirect_to tasks_url, notice: "タスク「 #{task.name} 」を削除しました。"
   end
@@ -40,4 +38,8 @@ class TasksController < ApplicationController
     def task_params
       params.require(:task).permit(:name, :description)
     end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
 end
