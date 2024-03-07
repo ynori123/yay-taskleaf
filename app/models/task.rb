5ecf0af470b44d1ed07e28b42de14342ec4c02f7
se4weed <norisama1159@gmail.com>
2024-03-07 14:22:52 +0900

    callbackの実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,9 +2,15 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 30 }
   validate :validate_name_not_including_comma
+  before_validation :set_nameless_name
 
   private
     def validate_name_not_including_comma
       errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+    end
+  
+  private
+    def set_nameless_name
+      self.name = '名前なし' if name.blank?
     end
 end
