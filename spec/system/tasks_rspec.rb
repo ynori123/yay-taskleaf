0867ffc39f693b9847bcc65ac667639c5962a95d
se4weed <norisama1159@gmail.com>
2024-03-07 20:02:07 +0900

    Taskのspecを作成

---
+++ spec/system/tasks_rspec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーを作成しておく
+      # 作成者がユーザーAであるタスクを作成しておく
+    end
+
+    context 'ユーザーAがログインしている時' do
+      # ユーザーAでログインする
+    end
+
+    it 'ユーザーAが作成したタスクが表示される' do
+      # 作成済みのタスクの名称が画面上に表示されていることを確認
+    end
+  end
+end
