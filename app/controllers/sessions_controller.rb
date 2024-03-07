65646bef42593c2939c4b3a5ed0388bb4a78aa73
se4weed <norisama1159@gmail.com>
2024-03-07 15:41:44 +0900

    ログイン処理の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,20 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def index
+    user = User.find_by(email: params[:email])
+    
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "ログインしました。"
+    else
+      render :new
+    end
+  end
+
+  private
+    def session_params
+      params.require(:session).permit(:email, :password)
+    end
 end
