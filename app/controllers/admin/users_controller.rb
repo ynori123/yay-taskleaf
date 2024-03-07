be92952560fe0fca35b842ae4cf735db578edc6c
se4weed <norisama1159@gmail.com>
2024-03-07 14:37:28 +0900

    new,createアクションの実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,17 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_users_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
   def edit
   end
 
@@ -10,4 +20,9 @@ class Admin::UsersController < ApplicationController
 
   def index
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+    end
 end
