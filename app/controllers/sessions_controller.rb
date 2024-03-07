acd128310d538ccb46293debd801cd23ba52269a
se4weed <norisama1159@gmail.com>
2024-03-07 18:56:13 +0900

    indexアクションをcreateアクションに変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,14 @@ class SessionsController < ApplicationController
   def new
   end
 
-  def index
+  def create
     user = User.find_by(email: params[:email])
     
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to root_url, notice: "ログインしました。"
     else
-      render :new
+      render :new, notice: "ログインに失敗しました。"
     end
   end
 
