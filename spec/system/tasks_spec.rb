5d06b5a78e59936d72fa49ac3422502de9d59da5
se4weed <norisama1159@gmail.com>
2024-03-08 10:49:55 +0900

    詳細表示機能のspecを追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -5,11 +5,9 @@ describe 'タスク管理機能', type: :system do
     # ユーザーを作成しておく
     let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
     let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+    let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
     before do
-      # 作成者がユーザーAであるタスクを作成しておく
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-
       visit login_path
       fill_in 'session[email]', with: login_user.email
       fill_in 'session[password]', with: login_user.password
@@ -29,6 +27,20 @@ describe 'タスク管理機能', type: :system do
       it 'ユーザーAが作成したタスクが表示されない' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
         expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしている時' do
+      let(:login_user) { user_a }
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
