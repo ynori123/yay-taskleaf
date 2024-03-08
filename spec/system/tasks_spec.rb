8946fe1025f86550460d1002dbfcb956c9a7820f
se4weed <norisama1159@gmail.com>
2024-03-08 09:37:23 +0900

    ファイル名の変更: tasks_rspec.rb -> tasks_spec.rb

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -19,6 +19,7 @@ describe 'タスク管理機能', type: :system do
       end
       it 'ユーザーAが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
