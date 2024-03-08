fcf567c01a5fe8f5b3519a2a3078af1cb018effe
se4weed <norisama1159@gmail.com>
2024-03-08 12:25:41 +0900

    'ユーザーAが作成したタスクが表示される'を共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -14,12 +14,13 @@ describe 'タスク管理機能', type: :system do
       click_button 'ログインする'
     end
 
+    shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+      it { expect(page).to have_content '最初のタスク' }
+    end
+
     context 'ユーザーAがログインしている時' do
       let(:login_user) { user_a }
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしている時' do
@@ -39,9 +40,7 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
