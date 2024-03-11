7c5b168690242cf239ef1c014f2521f039185b2b
se4weed <norisama1159@gmail.com>
2024-03-11 11:12:05 +0900

    タスクのCSVインポート機能の実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -22,6 +22,14 @@ class Task < ApplicationRecord
     end
   end
 
+  def self.import(file)
+    CSV.foreach(file.path, headers: :true) do |row|
+      task = new
+      task.attributes = row.to_hash.slice(*csv_attributes)
+      task.save!
+    end
+  end
+
   def self.ransackable_attributes(auth_object = nil)
     %w[created_at name] # 検索可能にしたい属性をここに追加します
   end
